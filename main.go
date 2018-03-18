0c5f5834b900a9084b2190c6df6189b2cf1edbf1
lsaether <Lsaether@protonmail.com>
2018-03-18 20:55:15 +0100

    added some data

--- main.go
+++ main.go
@@ -6,6 +6,27 @@ import (
 	"time"
 )
 
+const (
+	// LineOldYin - A broken line changing into a solid line.
+	LineOldYin = 6
+
+	// LineYoungYang - A solid line.
+	LineYoungYang = 7
+
+	// LineYoungYin - A broken line.
+	LineYoungYin = 8
+
+	// LineOldYang - A solid line changing into a broken line.
+	LineOldYang = 9
+)
+
+const (
+	// Yin ..
+	Yin = false
+	// Yang ..
+	Yang = true
+)
+
 // Line represents a single coin toss for the IChing.
 type Line struct {
 	CoinFlips [3]string
@@ -14,16 +35,44 @@ type Line struct {
 
 func (l *Line) toString() string {
 	switch l.Total {
-	case 6:
+	case LineOldYin:
 		return "--X--"
-	case 7:
+	case LineYoungYang:
 		return "-----"
-	case 8:
+	case LineYoungYin:
 		return "-- --"
-	case 9:
+	case LineOldYang:
 		return "--0--"
 	}
 	return "uninitialized"
+}
+
+func (l *Line) toBool() bool {
+	if l.Total == LineOldYin || l.Total == LineYoungYin {
+		return Yin
+	} else if l.Total == LineOldYang || l.Total == LineYoungYang {
+		return Yang
+	} else {
+		panic("Not Yin or Yang.")
+	}
+}
+
+type Hexagram struct {
+	// Number given to this hexagram.
+	Number int
+	// The series of lines that compose this hexagram.
+	Lines [6]Line
+	// Name of the hexagram
+	Name string
+	// The unicode character of the hexagram.
+	Character string
+	// Description
+	Description string
+}
+
+// Reading ..
+type Reading struct {
+	Lines [6]Line
 }
 
 func flipCoin() string {
